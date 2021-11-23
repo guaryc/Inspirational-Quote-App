64d9425fca70f7b3afa9c854581a78c9fd1c8d47
guaryc <93789303+guaryc@users.noreply.github.com>
2021-11-23 13:53:52 -0500

    Add files via upload

---
+++ Positive Affirmation App.py
@@ -0,0 +1,17 @@
+value1 = input("What's your name?")
+print("Hi" + value1)
+def random():
+    from random import random
+#def choice():
+    from choice import Menu
+Menu = []
+phrase1 = "you are more than capable of accomplishing your goals"
+phrase2 = "today is your day"
+phrase3 = "you are beautiful inside and outside"
+phrase4 = "stay focused and never back down from adversity"
+phrase5 = "no one can you better than you"
+phrase6 = "your value in this world can't be measured'"
+phrase7 = "you are attractive inside and outside"
+phrase8 = "your past is behind you for a reason, learn it and be better today amd tomorrow"
+value2 = (phrase1, phrase2, phrase3, phrase4, phrase5, phrase6, phrase7, phrase8)
+print("No matter what, remember this", (value1), (value2))
